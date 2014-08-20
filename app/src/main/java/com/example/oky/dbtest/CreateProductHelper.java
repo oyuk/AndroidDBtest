3de96ef50692bd4864419b28ec548f0a8448a623
oky <reidreid68@gmail.com>
2014-08-20 13:38:02 +0900

    add ListView

---
+++ app/src/main/java/com/example/oky/dbtest/CreateProductHelper.java
@@ -0,0 +1,30 @@
+package com.example.oky.dbtest;
+
+import android.content.Context;
+import android.database.DatabaseErrorHandler;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by oky on 2014/08/20.
+ */
+public class CreateProductHelper extends SQLiteOpenHelper{
+
+
+    public CreateProductHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+
+
+    }
+}
--- app/src/main/java/com/example/oky/dbtest/MyActivity.java
+++ app/src/main/java/com/example/oky/dbtest/MyActivity.java
@@ -1,20 +1,78 @@
 package com.example.oky.dbtest;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class MyActivity extends Activity {
+
+    ListView listView;
+    Button button;
+    ArrayList<String> labelList;
+    ArrayAdapter<String> adapter;
+
+
+    /* データベース名 */
+    private final static String DB_NAME = "DBTest";
+    /* データベースのバージョン */
+    private final static int DB_VER = 1;
+    /* コンテキスト */
+    private Context mContext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
 
+        findViews();
+        setListeners();
+        setAdapters();
+
+        CreateProductHelper cph = new CreateProductHelper(this,DB_NAME,null,DB_VER);
 
 
+    }
+
+    private void findViews(){
+        listView = (ListView)findViewById(R.id.listView);
+        button = (Button)findViewById(R.id.button);
+    }
+
+    protected void setListeners(){
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switch (v.getId()){
+                    case R.id.button:
+
+                        break;
+                }
+
+            }
+        });
+
+    }
+
+    protected void setAdapters(){
+        labelList = new ArrayList<String>();
+
+        for (int i=0;i <= 20;i++){
+            labelList.add("item" + i);
+        }
+
+        adapter = new ArrayAdapter<String>(MyActivity.this,R.layout.cell,labelList);
+        listView.setAdapter(adapter);
     }
 
 
